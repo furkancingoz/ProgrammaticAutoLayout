ae3a55e151f74c62a6c5b4b6c118b27bac4cff90
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-01-15 22:29:52 +0300

    set project

--- ProgrammaticAutoLayout/ViewController.swift
+++ ProgrammaticAutoLayout/ViewController.swift
@@ -4,16 +4,42 @@
 //
 //  Created by Furkan Cingöz on 15.01.2024.
 //
-
+import SwiftUI
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    self.view.backgroundColor = .systemCyan
+
+
   }
+
+
 
 
 }
 
+
+
+
+
+
+
+
+
+#Preview {
+  ViewControllerViewRepresentable()
+}
+struct ViewControllerViewRepresentable: UIViewControllerRepresentable {
+  func makeUIViewController(context: Context) -> ViewController {
+
+    return ViewController()
+  }
+
+  func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+
+  }
+}
